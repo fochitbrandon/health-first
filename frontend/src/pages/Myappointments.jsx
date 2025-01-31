db251f6e76357c66c6dbebdff428214067a05531
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2025-01-31 16:38:55 +0000

    stashed some changes;

--- frontend/src/pages/Myappointments.jsx
+++ frontend/src/pages/Myappointments.jsx
@@ -1,14 +1,14 @@
 
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext';
-const Myappointments = () => {
+const Myappointments = () => {    
 const {doctors} = useContext(AppContext)
 
   return (
     <div>
       <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
         {" "}
-        My appointments
+       My appointments
       </p>
       <div>
         {doctors.slice(0, 3).map((item, index) => (
@@ -16,7 +16,7 @@ const {doctors} = useContext(AppContext)
             className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b "
             key={index}
           >
-            <div>
+            <div> 
               <img className="w-32 bg-red-50" src={item.image} alt="" />
             </div>
             <div className=" flex-1 text-sm text-zinc-600">
